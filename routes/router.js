6523fbe7ab14be63db2bf33c01d35ee1b16207cf
chan-kim <chan.ckeem@gmail.com>
2022-08-13 19:28:40 +0900

    create javascripts folder

---
+++ routes/router.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res) {
+  // send all data
+  res.render("index", { data: data });
+});
+
+router.get("/detail/:id", function (req, res) {
+  // send One data
+  res.render("detail");
+});
+
+router.get("/libraryRegister", function (req, res) {
+  res.render("write");
+});
+
+router.post("/bookshelfRegister", function (req, res) {
+  //create data
+  res.redirect("/");
+});
+
+router.get("/update", function (req, res) {
+  res.render("update");
+});
+
+router.post("/update", function (req, res) {
+  // patch data
+  res.redirect("/");
+});
+
+router.delete("/update", function (req, res) {
+  // delete data
+  res.redirect("/");
+});
+
+module.exports = router;
