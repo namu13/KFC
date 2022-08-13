1040d2ec86e344ee2ab02bc92c8d9c585e271e06
STHyeon <toby2009@naver.com>
2022-08-14 00:20:47 +0900

    feat(main): add create action

---
+++ public/js/actions.js
@@ -0,0 +1,29 @@
+function addLibrary() {
+  const userName = $("#card_user").val();
+  const bookshelf = $("#card_title").val();
+  const introduce = $("#card_description").val();
+  const password = $("#password").val();
+
+  var formData = {
+    userName,
+    bookshelf,
+    password,
+    introduce,
+    views: 0,
+    books: [],
+  };
+
+  $.ajax({
+    cache: false,
+    url: "/libraryRegister",
+    type: "POST",
+    data: formData,
+    success: function (data) {
+      console.log("succs");
+    },
+
+    error: function (xhr, status) {
+      alert(status);
+    },
+  });
+}
--- public/js/modal.js
+++ public/js/modal.js
@@ -1,9 +1,11 @@
 $(function () {
   $("#open_modal").click(function () {
+    $(".modal").css("overflowY", "hidden");
     $(".modal").fadeIn();
   });
 
   $("#close_close").click(function () {
+    $(".modal").css("overflowY", "auto");
     $(".modal").fadeOut();
   });
 });
