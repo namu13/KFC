a0d542b1fbf262de03589826481248f0d26c41ab
STHyeon <toby2009@naver.com>
2022-08-14 03:46:58 +0900

    feat(add_book): add book pug file

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,14 +1,12 @@
 $(function () {
-  console.log("object");
-
-  $("#open_modal").click(function () {
+  $(".open_modal").click(function () {
     const modal = $(this).data("modal");
 
     $(`#${modal}_modal`).css("overflowY", "hidden");
     $(`#${modal}_modal`).fadeIn();
   });
 
-  $("#close_close").click(function () {
+  $(".close_close").click(function () {
     const modal = $(this).data("modal");
 
     $(`#${modal}_modal`).css("overflowY", "auto");
